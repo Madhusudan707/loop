8dc7f9ba3ef03e9ccc2fbb63538a167f939fea7f
XDEV200 <xdev200200@gmail.com>
2021-07-10 07:58:46 +0530

    add: remote api endpoint

--- src/utility/baseURL.js
+++ src/utility/baseURL.js
@@ -2,6 +2,6 @@ const host = window.location.hostname;
     const localDomain = "http://localhost:";
     const port = "3003";
     const local = `${localDomain}${port}`;
-    const remote = "";
+    const remote = "https://loop-backend.madhusudandas.repl.co/";
   
 export    const baseURL = host === "localhost" ? local : remote;
