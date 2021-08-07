595f7a7b864d71197f90120112e6ac634e1d62cd
XDEV200 <xdev200200@gmail.com>
2021-08-07 21:30:44 +0530

    fix:api url

--- src/utility/baseURL.js
+++ src/utility/baseURL.js
@@ -2,6 +2,6 @@ const host = window.location.hostname;
     const localDomain = "http://localhost:";
     const port = "3003";
     const local = `${localDomain}${port}`;
-    const remote = "https://loop-backend.madhusudandas.repl.co/";
+    const remote = "https://loop-backend.madhusudandas.repl.co";
   
 export    const baseURL = host === "localhost" ? local : remote;
